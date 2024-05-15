4db1637bc6ffb4afdc6a85e951f4ce07e458d16e
Max van der Ree <maxvdree@hotmail.com>
2024-05-15 21:49:26 +0200

    Add dynamics member to Tests-class

--- source/tests.h
+++ source/tests.h
@@ -4,7 +4,21 @@
 class Tests {
 public:
 	Tests() {};
-	bool test_quadratic_solver(vector<string>& failed_tests, int verbosity) {
+	Tests(int _timestep, float _cell_width, float _self_ignition_factor, float _rainfall, float _seed_bearing_threshold,
+		float _growth_rate_multiplier, float _unsuppressed_flammability, float _min_suppressed_flammability, float _max_suppressed_flammability,
+		float _radius_suppr_flamm_min, float radius_range_suppr_flamm, float _max_dbh, float _saturation_threshold, float _fire_resistance_argmin,
+		float _fire_resistance_argmax, float _fire_resistance_stretch, float _background_mortality, map<string, map<string, float>> _strategy_distribution_params,
+		float _resource_grid_relative_size, float _mutation_rate, int _verbosity, int grid_size, float dbh_q1, float dbh_q2
+	) {
+		dynamics = Dynamics(_timestep, _cell_width, _self_ignition_factor, _rainfall, _seed_bearing_threshold,
+			_growth_rate_multiplier, _unsuppressed_flammability, _min_suppressed_flammability, _max_suppressed_flammability,
+			_radius_suppr_flamm_min, radius_range_suppr_flamm, _max_dbh, _saturation_threshold, _fire_resistance_argmin,
+			_fire_resistance_argmax, _fire_resistance_stretch, _background_mortality, _strategy_distribution_params,
+			_resource_grid_relative_size, _mutation_rate, _verbosity
+		);
+		dynamics.init_state(grid_size, dbh_q1, dbh_q2);
+		verbosity = _verbosity;
+	}
 		bool success = false;
 
 		// Cases
@@ -37,5 +51,7 @@ public:
 		}
 		else printf("All tests (%i) successful.", successes);
 	}
+	Dynamics dynamics;
+	int verbosity = 0;
 };
 
