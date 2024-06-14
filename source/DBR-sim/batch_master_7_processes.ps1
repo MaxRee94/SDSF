adb1f47767ad2cd2cc5348cd5ee14561919776b9
Max van der Ree <maxvdree@hotmail.com>
2024-06-14 12:25:08 +0200

    Add powershell scripts to start multiple separate batch processes

---
+++ source/DBR-sim/batch_master_7_processes.ps1
@@ -0,0 +1,9 @@
+﻿python ./create_batchfolder_lookup_table.py --number_of_batchfolders 10
+Start-Process batch_arguments.bat -ArgumentList @(0, 7)
+Start-Process batch_arguments.bat -ArgumentList @(1, 7)
+Start-Process batch_arguments.bat -ArgumentList @(2, 7)
+Start-Process batch_arguments.bat -ArgumentList @(3, 7)
+Start-Process batch_arguments.bat -ArgumentList @(4, 7)
+Start-Process batch_arguments.bat -ArgumentList @(5, 7)
+Start-Process batch_arguments.bat -ArgumentList @(6, 7)
+
