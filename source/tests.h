d33db97c6ca2f2455e0a83731a3cbe29910612c2
Max van der Ree <maxvdree@hotmail.com>
2024-07-02 12:13:27 +0200

    Rename variable

--- source/tests.h
+++ source/tests.h
@@ -8,13 +8,13 @@ public:
 		float _growth_rate_multiplier, float _unsuppressed_flammability, float _min_suppressed_flammability, float _max_suppressed_flammability,
 		float _radius_suppr_flamm_min, float radius_range_suppr_flamm, float _max_dbh, float _saturation_threshold, float _fire_resistance_argmin,
 		float _fire_resistance_argmax, float _fire_resistance_stretch, float _background_mortality, map<string, map<string, float>> _strategy_distribution_params,
-		float _resource_grid_relative_size, float _mutation_rate, float _fecundity_multiplier, int _verbosity, int grid_size, float dbh_q1, float dbh_q2
+		float _resource_grid_relative_size, float _mutation_rate, float _STR, int _verbosity, int grid_size, float dbh_q1, float dbh_q2
 	) {
 		dynamics = Dynamics(_timestep, _cell_width, _self_ignition_factor, _rainfall, _seed_bearing_threshold,
 			_growth_rate_multiplier, _unsuppressed_flammability, _min_suppressed_flammability, _max_suppressed_flammability,
 			_radius_suppr_flamm_min, radius_range_suppr_flamm, _max_dbh, _saturation_threshold, _fire_resistance_argmin,
 			_fire_resistance_argmax, _fire_resistance_stretch, _background_mortality, _strategy_distribution_params,
-			_resource_grid_relative_size, _mutation_rate, _fecundity_multiplier, _verbosity
+			_resource_grid_relative_size, _mutation_rate, _STR, _verbosity
 		);
 		dynamics.init_state(grid_size, dbh_q1, dbh_q2);
 		verbosity = _verbosity;
