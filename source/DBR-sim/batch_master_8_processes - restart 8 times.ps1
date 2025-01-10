0666b503a68238bae1c6aed027ef583000dd65a0
Max van der Ree <maxvdree@hotmail.com>
2025-01-10 11:16:13 +0100

    Script to restart a batch 8 times

---
+++ source/DBR-sim/batch_master_8_processes - restart 8 times.ps1
@@ -0,0 +1,72 @@
+﻿python ./create_batchfolder_lookup_table.py --number_of_batchfolders 10
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 8)
+Start-Process batch_arguments.bat -ArgumentList @(1, 8)
+Start-Process batch_arguments.bat -ArgumentList @(2, 8)
+Start-Process batch_arguments.bat -ArgumentList @(3, 8)
+Start-Process batch_arguments.bat -ArgumentList @(4, 8)
+Start-Process batch_arguments.bat -ArgumentList @(5, 8)
+Start-Process batch_arguments.bat -ArgumentList @(6, 8)
+Start-Process batch_arguments.bat -ArgumentList @(7, 8)
---
+++ source/DBR-sim/batch_master_singleprocess - restart 8 times.ps1
@@ -0,0 +1,6 @@
+﻿python ./create_batchfolder_lookup_table.py --number_of_batchfolders 10
+Start-Process batch_arguments.bat -ArgumentList @(0, 1) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 1) -Wait
+
+Start-Process batch_arguments.bat -ArgumentList @(0, 1)
